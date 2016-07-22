4f728143fa6cc8f602df1fa71ce2c989b818d1c5
Charles Zheng <charleszheng44@gmail.com>
2016-07-22 16:20:29 -0400

    add resource checker

--- mf_mesos_setting.py
+++ mf_mesos_setting.py
@@ -3,6 +3,7 @@
 # This module defined global variables and classes 
 # used by mf_mesos_scheduler.py
 
+import os
 import Queue
 import threading
 
@@ -13,6 +14,24 @@ lock = threading.RLock()
 offers_queue = Queue.Queue()
 # default makeflow working directory
 mf_wk_dir = "."
+DEBUG_FILE = "debug_mesos"
+SLEEP = 0
+
+def print_task_id_state():
+    if os.path.isfile(DEBUG_FILE):
+        debug_fn = open(DEBUG_FILE, "a+")
+    else:
+        debug_fn = open(DEBUG_FILE, "w+")
+
+    if len(tasks_info_dict) > 0:
+
+        for value in tasks_info_dict.itervalues():
+            debug_fn.write("{}:{},".format(value.task_id, value.action))
+
+        debug_fn.write("================\n")
+
+    debug_fn.close()
+    
 
 # Makeflow Mesos task info class
 class MfMesosTaskInfo:
